059f664e5e69bf54790fe38271d8dced6f005b57
temycodes <temycodes@gmail.com>
2025-03-01 09:47:45 +0100

    Add task: Checks if a number is even or odd

---
+++ 02evenOdd.js
@@ -0,0 +1,13 @@
+//Write a program that checks if a number is even or odd.
+//using if and else statements
+
+function checkEvenorOdd(number) {
+    if (number % 3 === 0) {
+        return "even number";
+    } else {
+        return "odd number";
+    }
+};
+
+console.log(checkEvenorOdd(3)); // returns odd
+console.log(checkEvenorOdd(2)); // returns even
