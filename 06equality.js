3fed2e2fe364ddd835a27b736373608e2abfa3e5
temycodes <temycodes@gmail.com>
2025-03-02 06:58:41 +0100

    Add checkPriceDiscount function to determine discount percentage based on purchase amount

---
+++ 06equality.js
@@ -0,0 +1,12 @@
+//Write a program that calculates a discount based on the purchase amount. Prices equal or over 100 discount have a discount of 20. Prices equal or over 50 have a discount of 10. Otherwise discount is 0
+
+function checkPriceDiscount(price) {
+  if (price >= 100) return "Discount is 20%";
+  if (price >= 50) return "Discount is 10%";
+  return "No discount";
+}
+
+//test case
+console.log(checkPriceDiscount(100)); //Discount is 20%
+console.log(checkPriceDiscount(50)); //Discount is 10%
+console.log(checkPriceDiscount(10)); //No discount
